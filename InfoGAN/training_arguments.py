0c94b4f272e7f1964a1a955777918c8c472fa2ac
Paul_TraxRetail <paul@traxretail.com>
2020-11-01 17:12:33 +0200

    visualization

--- InfoGAN/training_arguments.py
+++ InfoGAN/training_arguments.py
@@ -8,7 +8,7 @@ class Arguments:
     noise_dim: int = 89
     num_classes: int = 10
     num_examples_to_generate: int = 25
-    epochs: int = 6
+    epochs: int = 256
     init_lr: float = 0.0002
     visualization_folder: str = './training_visualization'
     check_points: str = './check_points'
